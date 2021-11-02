458bdeb2104f53f65e00f061ef9a373a81b87351
Binarylsy@Gmail.com <Binarylsy@Gmail.com>
2021-11-02 17:13:37 +0800

    test2

---
+++ app/src/main/java/com/example/myapplication1/MainActivity.java
@@ -0,0 +1,53 @@
+package com.example.myapplication1;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.widget.EditText;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class MainActivity extends AppCompatActivity {
+    private RecyclerView mRecyclerView;
+    private SearchAdapter mSearchAdapter = new SearchAdapter();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mRecyclerView = findViewById(R.id.rv);
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+        mRecyclerView.setAdapter(mSearchAdapter);
+        EditText et = findViewById(R.id.query);
+        et.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence sequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence sequence, int i, int i1, int i2) {
+                mSearchAdapter.getFilter().filter(sequence.toString());
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+
+        });
+
+        List<String> items = new ArrayList<>();
+        for (int i = 0; i < 100; i++) {
+            items.add("这是第"+String.valueOf(i)+"行");
+        }
+        mSearchAdapter.notifyItems(items);
+    }
+
+}
